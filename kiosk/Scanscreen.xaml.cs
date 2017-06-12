bfc494d23f62f8871434d33acba0496642a0f6ca
AndrewMurchison <VPA1737@VPA1737.valetparkofamerica.com>
2017-06-12 12:36:37 -0400

    Updated media to restart and added in the correct time

--- kiosk/Scanscreen.xaml.cs
+++ kiosk/Scanscreen.xaml.cs
@@ -23,6 +23,23 @@ namespace kiosk
         public Scanscreen()
         {
             InitializeComponent();
+            var dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+        
+        public void replayvid(object sender, EventArgs e)
+        {
+            med.Position = TimeSpan.FromSeconds(0);
+            med.Play();
+        }
+
+        public void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+           
+            time.Text = DateTime.Now.ToString("h:mm:ss tt");
+            
         }
         
        
